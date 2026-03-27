fb0e0eac6263b50e5cbfd7db6c9228e7c86db7f8
pugalabishek <pugalabishek1042006_bcs27@mepcoeng.ac.in>
2026-03-28 00:06:03 +0530

    Time: 0 ms (100%), Space: 43 MB (16.37%) - LeetHub

---
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        boolean zero=false;
+        for(int i=0;i<s.length();i++){
+            
+
+            if(s.charAt(i)=='0'){
+                zero=true;
+            }
+            if(s.charAt(i)=='1'&& zero)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
