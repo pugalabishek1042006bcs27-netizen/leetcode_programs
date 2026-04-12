b05db23f93e1baef3201ce18b1d6526f97550ec9
pugalabishek <pugalabishek1042006_bcs27@mepcoeng.ac.in>
2026-04-12 12:10:42 +0530

    Time: 7 ms (74.23%), Space: 49.6 MB (64.62%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);   // sort letters
+            String key = new String(chars);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
