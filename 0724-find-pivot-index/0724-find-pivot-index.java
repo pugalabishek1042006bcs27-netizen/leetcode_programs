3f03cacbf61a7a030e31bc330b5af51e74d1e703
pugalabishek <pugalabishek1042006_bcs27@mepcoeng.ac.in>
2026-07-15 02:04:19 +0530

    Time: 1 ms (98.72%), Space: 47.1 MB (88.97%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        int rightsum=0,leftsum=0;
+        for(int i=0;i<nums.length;i++){
+            rightsum=sum-leftsum-nums[i];
+            if(leftsum==rightsum){
+                return i;
+            }
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+}
